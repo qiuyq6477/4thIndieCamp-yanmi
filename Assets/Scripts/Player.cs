5bfd5d2e5fef517b85773e491f680f1f6e568600
qiu <819685619.com>
2020-12-27 21:15:12 +0800

    替换图片

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum MOVEDIR
+{
+    Left,
+    Right,
+    Down,
+    Up,
+    RotateLeft,
+    RotateRight,
+}
+
+public struct Operation
+{
+    public MOVEDIR dir;
+    public int angle;
+    public Vector3 pos;
+}
+
+public class Player 
+{
+    public List<Block> blocks = new List<Block>();
+    private List<Block> templist = new List<Block>();
+    private int angle = 0;
+    private Vector3 pos;
+    
+    List<Vector3> pointList = new List<Vector3>();
+
+    public bool CanMove = true;
+    public bool undo = false;
+    public List<Vector3> GetAllPoint()
+    {
+        pointList.Clear();
+        foreach (var block in blocks)
+        {
+            pointList.AddRange(block.GetAllPoint());
+        }
+
+        return pointList;
+    }
+    
+    public void AddBlock(Block block)
+    {
+        if(!templist.Contains(block))
+            templist.Add(block);
+    }
+    public void AddBlockImmeditaly(Block block)
+    {
+        blocks.Add(block);
+    }
+    public void RemoveBlock(Block block)
+    {
+        blocks.Remove(block);
+    }
+    public void RemoveAllBlock()
+    {
+        foreach (var block in blocks)
+        {
+            GameObject.Destroy(block.gameObject);
+        }
+        blocks.Clear();
+    }
+    public void Rotate(int a, Vector3 p)
+    {
+        angle = a;
+        pos = p;
+    }
+    public void MoveBlock(MOVEDIR dir)
+    {
+        if (!CanMove) return;
+        AudioManager.PlayAudioEffectA(Resources.Load<AudioClip>("Audio/移动"));
+        blocks.Sort((block, block1) =>
+        {
+            switch (dir)
+            {
+                case MOVEDIR.Left:
+                {
+                    return block.transform.localPosition.x > block1.transform.localPosition.x ? 1 : -1;
+                }
+                case MOVEDIR.Right:
+                {
+                    return block.transform.localPosition.x < block1.transform.localPosition.x ? 1 : -1;
+                }
+                case MOVEDIR.Down:
+                {
+                    return block.transform.localPosition.y > block1.transform.localPosition.y ? 1 : -1;
+                }
+                case MOVEDIR.Up:
+                {
+                    return block.transform.localPosition.y < block1.transform.localPosition.y ? 1 : -1;
+                }
+            }
+            return 1;
+        });
+        for (int i = 0; i < blocks.Count; i++)
+        {
+            blocks[i].Move(dir);
+        }
+        GameManager.Instance.UpdateDebugInfo();
+        
+        blocks.AddRange(templist);
+        templist.Clear();
+        
+        if(!undo)
+            GameManager.Instance.DoOperation(dir);
+        
+        if (angle != 0)
+        {
+            var rotate = true;
+            foreach (var block in blocks)
+            {
+                if (!block.checkRotation(angle, pos))
+                {
+                    rotate = false;
+                    break;
+                }
+            }
+
+            if (rotate)
+            {
+                AudioManager.PlayAudioEffectA(Resources.Load<AudioClip>("Audio/旋转"));
+                foreach (var block in blocks)
+                {
+                    GameManager.Instance.StartCoroutine(block.Rotate(angle, pos));
+                }
+                if(!undo)
+                    GameManager.Instance.DoOperation(angle>0?MOVEDIR.RotateRight : MOVEDIR.RotateLeft, angle, pos);
+            }
+            //checkcollision
+            angle = 0;
+        }
+
+        if (GameManager.Instance.IsGameOver())
+        {
+            GameManager.Instance.NextLevel();
+        }
+
+        undo = false;
+    }
+}
